a337d613ff36790c78ec2ddd8d21ab1f045b9799
ryo <ryo.onodera20000914@gmail.com>
2023-03-13 21:57:05 +0900

    初めて変更しました

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">yoshihito, 水野未悠 ,レンand ○○○</span>.
+      <span class="names">yoshihito, 水野未悠 ,レン ,でらりょー</span>.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
